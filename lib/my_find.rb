4481e0fcb9cf9a030fb28b2524d40e7257ef9c6e
Dylan Michael Labat <dml252@live.com>
2019-01-21 16:19:20 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,9 +2,9 @@ require 'pry'
 
 def my_find(collection)
   i = 0
-  block_return_values = []
   while i < collection.length
     block_return_values << yield(collection[i])
+    return collection[i]
     i = i + 1
   end
   if block_return_values.include?(false)
